b8d88a1d11003f4ba56f73d2eaafb702f77265c2
Farmix2003 <ffarrux386@gmail.com>
2024-09-26 14:13:48 +0300

    Restaurant details: images, title, and description of restaurant.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { darkTheme } from "./theme/DarkTheme";
 import { Home, Navbar } from "./components";
 import { useTranslation } from "react-i18next";
+import RestaurantDetails from "./components/restaurant/RestaurantDetails";
 
 const App = () => {
   const { t, i18n } = useTranslation();
@@ -15,7 +16,8 @@ const App = () => {
       <ThemeProvider theme={darkTheme}>
         <CssBaseline />
         <Navbar i18n={i18n} languages={languages} />
-        <Home t={t} />
+        {/* <Home t={t} /> */}
+        <RestaurantDetails />
       </ThemeProvider>
     </div>
   );
--- src/components/restaurant/RestaurantDetails.tsx
+++ src/components/restaurant/RestaurantDetails.tsx
@@ -1,7 +1,52 @@
-import React from "react";
+import { CalendarMonth, LocationOn } from "@mui/icons-material";
+import { Grid2 } from "@mui/material";
 
 const RestaurantDetails = () => {
-  return <section></section>;
+  return (
+    <div className="px-5 lg:px-20">
+      <section>
+        <h3 className="text-gray-500 py-3 mt-10">
+          Home{">"}Lefke{">"}Liman Pizza
+        </h3>
+        <div className="">
+          <Grid2 container spacing={2}>
+            <Grid2>
+              <img
+                className="w-screen h-[40vh] "
+                src="https://avatars.mds.yandex.net/i?id=5f8152e6b4ef93e120567447d84e02ce2f7f40fb98a5cadf-4866923-images-thumbs&n=13"
+                alt="img"
+              />
+            </Grid2>
+            <Grid2>
+              <img
+                className="w-screen h-[40vh] object-fill"
+                src="https://avatars.mds.yandex.net/i?id=0dd4476e6d06d2b28ada1e943a8e9510-5214861-images-thumbs&n=13"
+                alt="img"
+              />
+            </Grid2>
+          </Grid2>
+        </div>
+        <div className="pt-3 pb-5">
+          <h1 className="text-4xl font-semibold">Liman Pizza</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
+            veritatis eius ex eaque quas! Soluta in dolor necessitatibus officia
+            exercitationem quisquam, non voluptas molestias porro corrupti,
+            repellendus laborum culpa sed.
+          </p>
+          <div className="flex gap-1 t-2">
+            <LocationOn />
+            Gemikona, Lefke
+          </div>
+          <div className="flex gap-1">
+            <CalendarMonth />
+            {/* <span className="mx-2">|</span> */}
+            <span>Open from 10:00 AM to 22:00 PM</span>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default RestaurantDetails;
